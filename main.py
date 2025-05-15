b67f353f97b65b314f14252f5cbea9891905f22d
eagletoe70 <64237875+eagletoe70@users.noreply.github.com>
2025-05-15 09:35:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,20 +14,28 @@ if left.button("밝은 느낌"):
             {"role": "system", "content": "너는 좀 낙천적이고 희망이 가득한 긍정주의자야"},
             {"role": "user", "content": content + "에 대한 시를 써줘"},
     ]
+    with st.spinner('시 작성 중...'):
+        result = llm.invoke(messages)
+        print(result.content)
+        st.write(result.content)
 if middle.button("몽환적 느낌"):
     messages = [
             {"role": "system", "content": "너는 좀 몽환적이고 별과 우주를 사랑하는 신비주의자야"},
             {"role": "user", "content": content + "에 대한 시를 써줘"},
     ]
+    with st.spinner('시 작성 중...'):
+        result = llm.invoke(messages)
+        print(result.content)
+        st.write(result.content)
 if right.button("어두운 느낌"):
     messages = [
             {"role": "system", "content": "너는 좀 비관적이고 시니컬한 염세주의자야"},
             {"role": "user", "content": content + "에 대한 시를 써줘"},
     ]
-with st.spinner('시 작성 중...'):
-    result = llm.invoke(messages)
-    print(result.content)
-    st.write(result.content)
+    with st.spinner('시 작성 중...'):
+        result = llm.invoke(messages)
+        print(result.content)
+        st.write(result.content)    
     
 # if st.button('시 작성 요청하기'):
 #     with st.spinner('시 작성 중...'):
